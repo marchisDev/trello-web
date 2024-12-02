6e7d5bc6ea501205ef5d7ef5a9ca3c15110caf8c
marchisDev <buiquangchicv0303@gmail.com>
2024-12-02 22:20:27 +0700

    config theme MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
 function App() {
-
   return (
     <>
       <div>marchisDev</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+
+      <Typography variant='body2' color='text.secondary'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+        magni excepturi, atque voluptas tenetur alias tempore harum fuga, qui
+        saepe earum omnis molestiae corporis blanditiis non quibusdam debitis
+        enim exercitationem!
+      </Typography>
+
+      <Button variant='text'>Text</Button>
+      <Button variant='contained'>Contained</Button>
+      <Button variant='outlined'>Outlined</Button>
 
       <AccessAlarmIcon />
       <ThreeDRotation />

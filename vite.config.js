b31f4145d8b4942a7566195ec453c69abb6054ed
marchisDev <buiquangchicv0303@gmail.com>
2025-02-05 23:39:55 +0700

    fix BUILD_MODE dev and production

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    // cho phep Vite su dung duoc process.env, mac dinh thi Vite khong ho tro, mac dinh dung import.meta.env
+    'process.env': process.env
+  },
   plugins: [react(), svgr()],
   // base: './'
   resolve: {
-    alias: [{ find: '~', replacement: '/src' }],
-  },
+    alias: [{ find: '~', replacement: '/src' }]
+  }
 })

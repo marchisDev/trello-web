40689b2a70c77bafe21af8d96942e9f72e9ecf2c
marchisDev <buiquangchicv0303@gmail.com>
2025-02-06 00:01:43 +0700

    fix BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ let apiRoot = ''
 if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
-if (process.env.BUILD_MODE === 'dev') {
+if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-kaaz.onrender.com'
 }
 export const API_ROOT = apiRoot

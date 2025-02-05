5d193d4c3caf1c3b81b7493fc47445e6585a7ee6
marchisDev <buiquangchicv0303@gmail.com>
2025-02-06 00:05:44 +0700

    update BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-kaaz.onrender.com'
+  apiRoot = 'https://trello-api-1i4c.onrender.com'
 }
 export const API_ROOT = apiRoot
 // export const API_ROOT = 'https://trello-api-kaaz.onrender.com'
